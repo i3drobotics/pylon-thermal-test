8919ef9aca168dde273c7b37ad0193c5b1944846
Ben Knight <benknight135@gmail.com>
2021-08-16 16:37:13 +0100

    update test script

--- README.md
+++ README.md
@@ -21,21 +21,31 @@ python run_test.py --option variable
 | output        | string  | Folderpath to store test results. Test data includes log file of results and any images captured during the test.                      | "."     |
 | rate          | integer | Data capture rate (frames per second). All data is captured in sync.                                                                   |  1      |
 | capture_temp  | bool    | Enable / disable capturing of temperature data from cameras during test.                                                               | true    |
-| capture_image | bool    | Enable / disable capturing of images during test.                                                                                      | true    |
+| save_images | bool    | Enable / disable saving of images during. Image will still be grabbed from camera but will not be saved to file.                                                                                    | true    |
 | left_serial   | string  | camera serial number for left camera. If not specified will connect to first two basler cameras found connected.                       | ""      |
 | right_serial  | string  | camera serial number for right camera. If not specified will connect to first two basler cameras found connected.                      | ""      |
 
 ### Results
 Each test will log will capture data at the capture rate specified and send the data to a log file. The name of this file will be generated using the unix timestamp and the left and right serial numbers of the camera used (e.g. TT_1629119898_40081086_40081087.txt). The format of this file is a comma seperated text file with the following information: 
-| Time | Left image filename | Right image filename | Left temperature | Right temperature |
-|------|---------------------|----------------------|------------------|-------------------|
-|      |                     |                      |                  |                   |
+| Time | Left image filename | Right image filename | Left temperature | Right temperature | Left grab success | Right grab success |
+|------|---------------------|----------------------|------------------|-------------------|-------------------|--------------------|
+|      |                     |                      |                  |                   |                   |                    |
 
 An example of the contents a file with this format is shown below:
 ```
-time,left_img,right_img,left_temp,right_temp
-1629119898,1629119898_l.png,1629119898_r.png,60.22,60.32
-1629120151,1629120151_l.png,1629120151_r.png,60.23,60.31
+time,left_img,right_img,left_temp,right_temp,left_success,right_success
+1629119898,1629119898_l.png,1629119898_r.png,60.22,60.32,Success,Success
+1629120151,1629120151_l.png,1629120151_r.png,60.23,60.31,Success,Success
+```
+
+Turning off capture_temp or capture_image will result in the same data format but columns will not be populated. For example the following is a file where image saving was disabled:
+```
+time,left_img,right_img,left_temp,right_temp,left_success,right_success
+1629119898,,,60.22,60.32,Success,Success
+1629120151,,,60.23,60.31,Success,Success
 ```
 
 If images are saved as part of the test then these will be saved in the same folder as the log file, named using the unix timestamp and '_l' for left camera and '_r' for right camera (e.g. '1629119898_l.png')
+
+## Future Work
+ - Phase SDK support
--- run_test.py
+++ run_test.py
@@ -1,30 +1,66 @@
 import argparse
 import sys
-from datetime import datetime
+import os
+import time
+from typing import NamedTuple
 from pypylon import pylon, genicam
+import cv2
 
-def parse_args():
+
+class TitaniaTestParams(NamedTuple):
+    left_serial: str
+    right_serial: str
+    output_folderpath: str
+    capture_rate: float
+    save_images: bool
+    capture_temperature: bool
+
+
+def parse_args() -> argparse.Namespace:
     # parse command line argument
     parser = argparse.ArgumentParser(description="Titania Testing")
     parser.add_argument('--output', type=str, default=".", help="\
         Folderpath to store test results. \n \
-        Test data includes log file of results and any images captured during the test.")
+        Test data includes log file of results and \
+        any images captured during the test.")
     parser.add_argument('--rate', type=float, default=1, help="\
         Data capture rate (frames per second). All data is captured in sync.")
     parser.add_argument('--capture_temp', type=bool, default=True, help="\
-        Enable / disable capturing of temperature data from cameras during test.")
-    parser.add_argument('--capture_image', type=bool, default=True, help="\
-        Enable / disable capturing of images during test.")
+        Enable / disable capturing of temperature data \
+        from cameras during test.")
+    parser.add_argument('--save_images', type=bool, default=True, help="\
+        Enable / disable saving of images during. \
+        Image will still be grabbed from camera but \
+        will not be saved to file.")
     parser.add_argument('--left_serial', type=str, default="", help="\
         Camera serial number for left camera. \
-        If not specified will connect to first two basler cameras found connected.")
+        If not specified will connect to first two \
+        basler cameras found connected.")
     parser.add_argument('--right_serial', type=str, default="", help="\
         Camera serial number for right camera. \
-        If not specified will connect to first two basler cameras found connected.")
+        If not specified will connect to first two \
+        basler cameras found connected.")
     args = parser.parse_args()
+    # Check arguments are valid
+    # If one camera serial is given then both must be given
+    left_serial_given = args.left_serial != ""
+    right_serial_given = args.right_serial != ""
+    if left_serial_given and not right_serial_given:
+        err_msg = "left_serial given without right_serial. \
+            Both left and right MUST be given if specifing camera serials."
+        raise Exception(err_msg)
+    if right_serial_given and not left_serial_given:
+        err_msg = "right_serial given without left_serial. \
+            Both left and right MUST be given if specifing camera serials."
+        raise Exception(err_msg)
+    # Check output path exists
+    if not os.path.exists(args.output):
+        err_msg = "Output path does not exist: " + args.output
+        raise Exception(err_msg)
     return args
 
-def getCameraSerials():
+
+def getCameraSerials() -> list:
     # Get camera serials of currently connected basler devices
     serial_list = []
     try:
@@ -34,10 +70,11 @@ def getCameraSerials():
         # Get all attached devices and exit application if no device is found.
         devices = tlFactory.EnumerateDevices()
         if len(devices) == 0:
-            raise pylon.RuntimeException("No camera present.")
+            return []
 
-        # Create an array of instant cameras for the found devices and avoid exceeding a maximum number of devices.
-        cameras = pylon.InstantCameraArray(min(len(devices), maxCamerasToUse))
+        # Create an array of instant cameras for the found devices
+        # and avoid exceeding a maximum number of devices.
+        cameras = pylon.InstantCameraArray(min(len(devices)))
 
         # Create and attach all Pylon Devices.
         for i, cam in enumerate(cameras):
@@ -53,91 +90,166 @@ def getCameraSerials():
         print("An exception occurred.", e)
         return []
 
+
+def getLogFileName() -> str:
+    # Get current unix time
+    unix_time = str(time.time())
+    # Create file name from unix time
+    log_file_name = "TT_" + unix_time + ".txt"
+    return log_file_name
+
+
+def run_test(test_params: TitaniaTestParams) -> int:
+    exit_code = 0
+    # Generate names for filepaths
+    log_filename = getLogFileName()
+    log_filepath = os.path.join(test_params.output_folderpath, log_filename)
+
+    try:
+        # Get the transport layer factory.
+        tlFactory = pylon.TlFactory.GetInstance()
+
+        # Get all attached devices and exit application if no device is found.
+        devices = tlFactory.EnumerateDevices()
+        if len(devices) == 0:
+            raise pylon.RuntimeException("No camera present.")
+
+        # Create an array of instant cameras
+        cameras = pylon.InstantCameraArray(min(len(devices), 2))
+
+        # Create and attach Pylon Devices.
+        # TODO attach to camera serials from params
+        # TODO left camera must be in index 0, right camera must be in index 1
+        for i, cam in enumerate(cameras):
+            cam.Attach(tlFactory.CreateDevice(devices[i]))
+
+            # Print the model name of the camera.
+            print("Using device ", cam.GetDeviceInfo().GetModelName())
+
+        # Start capture
+        cameras.StartGrabbing(
+            pylon.GrabStrategy_LatestImageOnly,
+            pylon.GrabLoop_ProvidedByUser)
+
+        # Set capture rate
+        for i, cam in enumerate(cameras):
+            acquisition_frame_rate = 1.0 / test_params.capture_rate
+            cam.AcquisitionFrameRate.SetValue(acquisition_frame_rate)
+            cam.AcquisitionFrameRateEnable.SetValue(True)
+
+        while True:
+            # Get capture time
+            capture_time = str(time.time())
+            # Define default values for log data
+            left_temp = ""
+            right_temp = ""
+            left_image_filename = ""
+            right_image_filename = ""
+            left_success = "Failed"
+            right_success = "Failed"
+            # Check cameras are grabbing
+            if cameras.IsGrabbing():
+                # Read camera data
+                grabResult_left = cameras[0].RetrieveResult(
+                    20000, pylon.TimeoutHandling_ThrowException)
+                grabResult_right = cameras[1].RetrieveResult(
+                    20000, pylon.TimeoutHandling_ThrowException)
+
+                # Check data capture success
+                grabSuccess_left = \
+                    grabResult_left.GetStatus() == pylon.GrabStatus_Success
+                grabSuccess_right = \
+                    grabResult_right.GetStatus() == pylon.GrabStatus_Success
+                # Save camera images to file
+                if grabSuccess_left:
+                    left_success = "Success"
+                    if test_params.save_images:
+                        img = grabSuccess_left.GetArray()
+                        left_image_filename = capture_time + "_l.png"
+                        left_image_filepath = os.path.join(
+                            test_params.output_folderpath,
+                            left_image_filename)
+                        cv2.imwrite(left_image_filepath, img)
+                if grabSuccess_right:
+                    right_success = "Success"
+                    if test_params.save_images:
+                        img = grabSuccess_right.GetArray()
+                        right_image_filename = capture_time + "_r.png"
+                        right_image_filepath = os.path.join(
+                            test_params.output_folderpath,
+                            right_image_filename)
+                        cv2.imwrite(right_image_filepath, img)
+
+                if test_params.capture_temperature:
+                    # Get temperature
+                    left_temp_data = cameras[0].DeviceTemperature.GetValue()
+                    right_temp_data = cameras[1].DeviceTemperature.GetValue()
+                    left_temp = str(left_temp_data)
+                    right_temp = str(right_temp_data)
+
+            # Define log message
+            # time,left_img,right_img,left_temp,right_temp,left_success,right_success
+            log_msg = capture_time \
+                + "," + left_image_filename + "," + right_image_filename + \
+                + "," + left_temp + "," + right_temp + \
+                + "," + left_success + "," + right_success + \
+                "\n"
+
+            # Write log message to file
+            f = open(log_filepath, "a")
+            f.write(log_msg)
+            f.close()
+
+    except genicam.GenericException as e:
+        # Error handling
+        print("An exception occurred.", e)
+        exit_code = 1
+
+    return exit_code
+
+
+def main() -> int:
+    exit_code = 0
+    # Get command line arguments
+    args = parse_args()
+    # Get currently connected devices
+    camera_serials = getCameraSerials()
+    # Check connected devices against arguments
+    left_serial = None
+    right_serial = None
+    if args["left_serial"] != "":
+        if args["left_serial"] not in camera_serials:
+            raise Exception("Left camera not found: " + args["left_serial"])
+        if args["right_serial"] not in camera_serials:
+            raise Exception("Right camera not found: " + args["right_serial"])
+        left_serial = args["left_serial"]
+        right_serial = args["right_serial"]
+    if args["left_serial"] == "":
+        if len(camera_serials) > 2:
+            raise Exception("Too many camera connected. \
+                Please specify --left_serial and --right_serial.")
+        if len(camera_serials) < 2:
+            raise Exception("Not enough camera connected. Found " +
+                            str(len(camera_serials)) + " cameras.")
+        left_serial = camera_serials[0]
+        right_serial = camera_serials[1]
+    if left_serial is None or right_serial is None:
+        # This shouldn't be possible as previous error checking
+        # should always set serials or raise an exception
+        raise Exception("Failed to get valid camera serials")
+    # Define test parameters
+    test_params = TitaniaTestParams(
+        left_serial=left_serial, right_serial=right_serial,
+        output_folderpath=args["output"],
+        capture_rate=args["rate"],
+        capture_images=args["capture_image"],
+        capture_temperature=args["capture_temp"]
+    )
+    # Run test
+    exit_code = run_test(test_params)
+    return exit_code
+
+
 if __name__ == "__main__":
-    pass
-
-left_serial = "40098270"
-right_serial = "40098281"
-log_filepath = "temp.log"
-
-# Number of images to be grabbed.
-countOfImagesToGrab = 10
-
-maxCamerasToUse = 2
-
-# The exit code of the sample application.
-exitCode = 0
-
-try:
-
-    # Get the transport layer factory.
-    tlFactory = pylon.TlFactory.GetInstance()
-
-    # Get all attached devices and exit application if no device is found.
-    devices = tlFactory.EnumerateDevices()
-    if len(devices) == 0:
-        raise pylon.RuntimeException("No camera present.")
-
-    # Create an array of instant cameras for the found devices and avoid exceeding a maximum number of devices.
-    cameras = pylon.InstantCameraArray(min(len(devices), maxCamerasToUse))
-
-    l = cameras.GetSize()
-
-    # Create and attach all Pylon Devices.
-    for i, cam in enumerate(cameras):
-        cam.Attach(tlFactory.CreateDevice(devices[i]))
-
-        # Print the model name of the camera.
-        print("Using device ", cam.GetDeviceInfo().GetModelName())
-
-    # Starts grabbing for all cameras starting with index 0. The grabbing
-    # is started for one camera after the other. That's why the images of all
-    # cameras are not taken at the same time.
-    # However, a hardware trigger setup can be used to cause all cameras to grab images synchronously.
-    # According to their default configuration, the cameras are
-    # set up for free-running continuous acquisition.
-    cameras.StartGrabbing()
-
-    for i, cam in enumerate(cameras):
-        cam.AcquisitionFrameRate.SetValue(0.1)
-        cam.AcquisitionFrameRateEnable.SetValue(True)
-
-    while True:
-        if not cameras.IsGrabbing():
-            break
-
-        grabResult = cameras.RetrieveResult(20000, pylon.TimeoutHandling_ThrowException)
-
-        # When the cameras in the array are created the camera context value
-        # is set to the index of the camera in the array.
-        # The camera context is a user settable value.
-        # This value is attached to each grab result and can be used
-        # to determine the camera that produced the grab result.
-        cameraContextValue = grabResult.GetCameraContext()
-
-        # Print the index and the model name of the camera.
-        print("Camera ", cameraContextValue, ": ", cameras[cameraContextValue].GetDeviceInfo().GetModelName())
-        
-        # Now, the image data can be processed.
-        print("GrabSucceeded: ", grabResult.GrabSucceeded())
-        img = grabResult.GetArray()
-
-        # Get temporature
-        temp = cameras[cameraContextValue].DeviceTemperature.GetValue()
-        print("Temp: ", temp)
-
-        timestamp = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
-        log_msg = str(cameraContextValue) + "," + timestamp + "," + str(temp) + "\n"
-
-        f = open(log_filepath, "a")
-        f.write(log_msg)
-        f.close()
-
-
-except genicam.GenericException as e:
-    # Error handling
-    print("An exception occurred.", e)
-    exitCode = 1
-
-# Comment the following two lines to disable waiting on exit.
-sys.exit(exitCode)
+    exit_code = main()
+    sys.exit(exit_code)
