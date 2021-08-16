f12ea0e7ea96e967a76ed450b9d8b155be98b5c1
Ben <benknight135@gmail.com>
2021-08-16 13:57:08 +0000

    update test script (in progress)

--- run_test.py
+++ run_test.py
@@ -1,8 +1,60 @@
-from pypylon import pylon
-from datetime import datetime
-from pypylon import genicam
-from pypylon import pylon
+import argparse
 import sys
+from datetime import datetime
+from pypylon import pylon, genicam
+
+def parse_args():
+    # parse command line argument
+    parser = argparse.ArgumentParser(description="Titania Testing")
+    parser.add_argument('--output', type=str, default=".", help="\
+        Folderpath to store test results. \n \
+        Test data includes log file of results and any images captured during the test.")
+    parser.add_argument('--rate', type=float, default=1, help="\
+        Data capture rate (frames per second). All data is captured in sync.")
+    parser.add_argument('--capture_temp', type=bool, default=True, help="\
+        Enable / disable capturing of temperature data from cameras during test.")
+    parser.add_argument('--capture_image', type=bool, default=True, help="\
+        Enable / disable capturing of images during test.")
+    parser.add_argument('--left_serial', type=str, default="", help="\
+        Camera serial number for left camera. \
+        If not specified will connect to first two basler cameras found connected.")
+    parser.add_argument('--right_serial', type=str, default="", help="\
+        Camera serial number for right camera. \
+        If not specified will connect to first two basler cameras found connected.")
+    args = parser.parse_args()
+    return args
+
+def getCameraSerials():
+    # Get camera serials of currently connected basler devices
+    serial_list = []
+    try:
+        # Get the transport layer factory.
+        tlFactory = pylon.TlFactory.GetInstance()
+
+        # Get all attached devices and exit application if no device is found.
+        devices = tlFactory.EnumerateDevices()
+        if len(devices) == 0:
+            raise pylon.RuntimeException("No camera present.")
+
+        # Create an array of instant cameras for the found devices and avoid exceeding a maximum number of devices.
+        cameras = pylon.InstantCameraArray(min(len(devices), maxCamerasToUse))
+
+        # Create and attach all Pylon Devices.
+        for i, cam in enumerate(cameras):
+            cam.Attach(tlFactory.CreateDevice(devices[i]))
+
+            # Print the model name of the camera.
+            cam_serial = cam.GetDeviceInfo().GetSerialNumber()
+            print("Serial: ", cam_serial)
+            serial_list.append(cam_serial)
+
+    except genicam.GenericException as e:
+        # Error handling
+        print("An exception occurred.", e)
+        return []
+
+if __name__ == "__main__":
+    pass
 
 left_serial = "40098270"
 right_serial = "40098281"
